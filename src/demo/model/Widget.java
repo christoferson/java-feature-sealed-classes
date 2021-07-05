90a91aa0f552f2d3ab4c3078b47a0b6e527292fb
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-07-05 22:17:15 +0900

    Add Medal

--- src/demo/model/Widget.java
+++ src/demo/model/Widget.java
@@ -6,6 +6,11 @@ import demo.others.Spinner;
 
 // Permitted type Spinner in an unnamed module should be declared in the same package demo.model of declaring type Widget
 //public sealed class Widget permits Button, Label, Spinner {
+
+//Sealed class or interface lacks the permits clause and no class or interface from the same compilation unit declares Widget as its direct superclass or superinterface
+//public sealed class Widget {
+
+
 public sealed class Widget permits Button, Label {
 	
 }
